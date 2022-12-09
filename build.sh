53deefa12cb43c7736b20a72435ee30513dec640
Liang Wang <netcasper@gmail.com>
2022-12-08 17:25:41 -0800

    Upgrade to 24.0.8 and fix default argument

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 set -e
 
 VERSION=${1:-latest}
-RESET_FACE_RECOGNITION={$2:no}
+RESET_FACE_RECOGNITION={$2:-no}
 
 docker build -t netcasper/nextcloud-ssl:${VERSION}-apache-ssl .
 docker push netcasper/nextcloud-ssl:${VERSION}-apache-ssl
 docker service update --image netcasper/nextcloud-ssl:${VERSION}-apache-ssl nextcloud_app
 if [ "${RESET_FACE_RECOGNITION}" != "no" ]
 then
+    echo ${RESET_FACE_RECOGNITION}
+    echo "reset face recognition"
     CONTAINER_ID=nextcloud_app.1.$(docker stack ps -f 'name=nextcloud_app.1' nextcloud -q --no-trunc | head -n1)
     docker exec -ti -u www-data ${CONTAINER_ID} php occ face:reset --all
     docker exec -ti -u www-data ${CONTAINER_ID} php occ face:setup -M 4G -m 1
